8af51a8f6d3e8b3233b38cec2508b98562f2d99a
yyf <yuyiyan@xiongmaitech.com>
2019-03-20 17:37:33 +0800

    初始化

---
+++ src/main/java/com/ivan/happytime/triplehappiness/TripleHappinessApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.happytime.triplehappiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleHappinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleHappinessApplication.class, args);
+    }
+
+}
